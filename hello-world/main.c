175d7aa8e83392a27838a622d12b7cb0feaf8bcb
John <andreygdsn@gmail.com>
2024-02-25 00:53:16 -0300

    comments

--- hello-world/main.c
+++ hello-world/main.c
@@ -3,6 +3,15 @@
 int main()
 {
     printf("Hello, World!\n");
-    printf("Hello, You!\n");
+    printf("Hello, You!\n");//print on terminal
+    /*multi
+    line
+    comment
+    
+    \n new lani
+    \t tab
+    */
+
+    printf("1\t2\t3\n4\t5\t6\n7\t8\t9");
     return 0;
 }
