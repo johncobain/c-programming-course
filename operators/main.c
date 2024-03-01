58f9f848bf285209d2482c934cd4403494b8b30e
John <andreygdsn@gmail.com>
2024-03-01 16:08:18 -0300

    constants and operators

---
+++ operators/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+    // +\-\*\/\%\++\--
+    int x = 5;
+    int y = 2;
+
+    x++;
+    x+=1;
+    float z = x/(float)y;
+
+    printf("%f", z);
+    return 0;
+}
