86e4bef5ed62aa76bd465ba59a32ace2bb69ff01
John <andreygdsn@gmail.com>
2024-03-05 17:43:00 -0300

    cMath

---
+++ cMath/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double A = sqrt(9);
+    double B = pow(2,4);
+    int C = round(3.14);
+    int D = ceil(3.14);
+    int E = floor(3.99);
+    double F = fabs(-100);
+    double G = log(3);
+    double H = sin(45);
+    double I = cos(45);
+    double J = tan(45);
+
+
+    printf("\n%1f",A);
+    printf("\n%1f",B);
+    printf("\n%d",C);
+    printf("\n%d",D);
+    printf("\n%d",E);
+    printf("\n%1f",F);
+    printf("\n%1f",G);
+    printf("\n%1f",H);
+    printf("\n%1f",I);
+    printf("\n%1f",J);
+
+    return 0;
+}
