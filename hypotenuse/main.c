2dcbd7d42956b86c6eb98604d1faebd8c1533ed6
John <andreygdsn@gmail.com>
2024-03-07 11:20:33 -0300

    hypotenuse

---
+++ hypotenuse/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(){
+
+    double a,b,c;
+
+    printf("Enter side A: ");
+    scanf("%lf", &a);
+    printf("Enter side B: ");
+    scanf("%lf", &b);
+
+    c = sqrt(a*a+b*b);
+
+    printf("Side C: %lf", c);
+
+    return 0;
+}
