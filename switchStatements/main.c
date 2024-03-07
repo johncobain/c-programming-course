516205b3f30693133ec44877720c1aaa9fd24e7d
John <andreygdsn@gmail.com>
2024-03-07 11:41:32 -0300

    switch statements

---
+++ switchStatements/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(){
+    char grade;
+    printf("\nEnter a letter grade: ");
+    scanf("%c", &grade);
+
+    switch (grade){
+    case 'A':
+        printf("Perfect!\n");
+        break;
+    case 'B':
+        printf("You did good!\n");
+        break;
+    case 'C':
+        printf("You did okay\n");
+        break;
+    case 'D':
+        printf("At least it's not an F\n");
+        break;
+    case 'F':
+        printf("YOU FAILED!\n");
+        break;
+    default:
+        printf("Please enter only valid grades!");
+        break;
+    }
+
+    return 0;
+}
