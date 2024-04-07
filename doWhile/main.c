e59ca19f2f23d2586fab3e9121118b106d2636de
John <andreygdsn@gmail.com>
2024-04-07 12:25:35 -0300

    do while loop

--- doWhile/main.c
+++ doWhile/main.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 
 int main(){
+    //do while loop
+    int number = 0;
+    int sum = 0;
 
-
+    do{
+        printf("Enter a number above 0: ");
+        scanf("%d", &number);
+        if(number>0){
+            sum += number;
+        }
+    }while(number>0);
+    printf("Sum: %d",sum);
     return 0;
 }
