504615027e14ab3ffdf2a05a7440d89f50a6f3c2
John <andreygdsn@gmail.com>
2024-04-07 12:04:11 -0300

    strings

--- stringFunctions/main.c
+++ stringFunctions/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 
 int main(){
+    char string1[] = "John";
+    char string2[] = "Cobain";
 
+    // strlwr(string1);
+    // strupr(string1);
+    strcat(string1, string2);
+    // strncat(string1,string2,1);
+    // strcpy(string1, string2);
+    // strncpy(string1,string2,4);
+
+    // strset(string1, '?');
+    // strnset(string1, 'x', 1);
+    // strrev(string1);
+
+    int result = strlen(string1);
+    // int result = strcmp(string1, string2);
+    // int result = strncmp(string1, string2,1);
+    // int result = strcmpi(string1, string1);
+    // int result = sctnicmp(string1, string1,1);
+
+
+    printf("\n%s",string1);
+    printf("\n%d",result);
 
     return 0;
 }
