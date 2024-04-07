7a81d672785e962efd0e04f46f483a9511956ecf
John <andreygdsn@gmail.com>
2024-04-07 11:46:52 -0300

    ternary

---
+++ ternary/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int findMax(int x, int y){
+    return (x>y)? x: y;
+}
+
+
+
+int main(){
+    //condition? value if true: value if false
+    int max = findMax(3,4);
+
+    printf("%d",max);
+
+    return 0;
+}
